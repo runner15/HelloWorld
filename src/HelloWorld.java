17bbfcc6a6a060ceb088bd375fe10858bddd422a
runner15 <therunner151@gmail.com>
2018-03-02 09:10:34 -0700

    Part 3 questions

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,3 +3,13 @@ public class HelloWorld {
 		System.out.println("Hello World Updated");
 	}
 }
+
+/*
+1. A pull is a git fetch and a git merge.  Brings local branch up to date to remote version.
+2. A fetch updates the remote tracking branches, and does not change local files.
+3. A clones copies an existing git repository.
+4. A push is moving local data to a remote repository.
+5. A branch is a fork of the repository.  It is its own codebase.
+6. To have multiple people code, or to work on different parts of the project, or different code bases for the same project.
+7. Git status gives information about the local git repository, inluding changed files and commits.
+*/
